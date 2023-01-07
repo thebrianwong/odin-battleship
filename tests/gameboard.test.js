4b91a3ea2816087e53854cc4ffe8129b59d23bd4
Brian Wong <brianw7218@gmail.com>
2023-01-06 22:32:01 -0800

    Create test tile for gameboard object and add some tests

---
+++ tests/gameboard.test.js
@@ -0,0 +1,72 @@
+const Ship = require("../src/ship");
+const Gameboard = require("../src/gameboard");
+
+test("create a gameboard object", () => {
+  const gameboard = Gameboard();
+  expect(typeof gameboard).toBe("object");
+});
+
+test("place a length 4 ship on the gameboard", () => {
+  const gameboard = Gameboard();
+  const ship = Ship(4);
+  gameboard.placeShip(ship, [
+    [3, 4],
+    [3, 5],
+    [3, 6],
+    [3, 7],
+  ]);
+  expect(gameboard.getCoordinates([3, 4])).toBe(ship);
+  expect(gameboard.getCoordinates([3, 5])).toBe(ship);
+  expect(gameboard.getCoordinates([3, 6])).toBe(ship);
+  expect(gameboard.getCoordinates([3, 7])).toBe(ship);
+  expect(gameboard.getCoordinates([3, 8])).toBeUndefined();
+});
+
+test("hit a ship on the gameboard", () => {
+  const gameboard = Gameboard();
+  const ship = Ship(4);
+  gameboard.placeShip(ship, [
+    [3, 4],
+    [3, 5],
+    [3, 6],
+    [3, 7],
+  ]);
+  gameboard.receiveAttack([3, 4]);
+  expect(gameboard.getCoordinates([3, 4])).toHaveBeenCalled();
+  expect(ship.hit()).toHaveBeenCalled(); /* keep for now but prob not */
+  expect(ship.getShipHits()).toBe(1);
+});
+
+test("hit was a miss", () => {
+  const gameboard = Gameboard();
+  const ship = Ship(4);
+  gameboard.placeShip(ship, [
+    [3, 4],
+    [3, 5],
+    [3, 6],
+    [3, 7],
+  ]);
+  gameboard.receiveAttack([0, 0]);
+  expect(gameboard.getCoordinates([0, 0])).toHaveBeenCalled();
+  expect(ship.getShipHits()).toBe(0);
+  expect(
+    gameboard.addToMissedShots([0, 0])
+  ).toHaveBeenCalled(); /* might be private */
+});
+
+test("get history of missed shots", () => {
+  const gameboard = Gameboard();
+  const ship = Ship(4);
+  gameboard.placeShip(ship, [
+    [3, 4],
+    [3, 5],
+    [3, 6],
+    [3, 7],
+  ]);
+  gameboard.receiveAttack([0, 1]);
+  gameboard.receiveAttack([0, 2]);
+  gameboard.receiveAttack([0, 3]);
+  expect(gameboard.getMissedShots()).toEqual(expect.arrayContaining([0, 0]));
+  expect(gameboard.getMissedShots()).toEqual(expect.arrayContaining([0, 1]));
+  expect(gameboard.getMissedShots()).toEqual(expect.arrayContaining([0, 2]));
+});
