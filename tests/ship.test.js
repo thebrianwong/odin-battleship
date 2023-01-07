f86ffb0f0fff7205dce39291167ad8ea6e724de9
Brian Wong <brianw7218@gmail.com>
2023-01-06 21:21:29 -0800

    Create some tests for the ship object

---
+++ tests/ship.test.js
@@ -0,0 +1,39 @@
+const Ship = require("../src/ship");
+
+test("create a ship with 4 length, 0 hit counter, and not sunk", () => {
+  const ship = Ship(4);
+  expect(ship).toStrictEqual({
+    getShipLength,
+    getShipHits,
+    hit,
+    isSunk,
+  });
+  expect(ship.getShipLength()).toBe(4);
+  expect(ship.getShipHits()).toBe(0);
+  expect(ship.isSunk()).toBeFalsy();
+});
+
+test("create a ship with 3 length, hit it 2 times, increasing hits to 2, but not sunk", () => {
+  const ship = Ship(3);
+  ship.hit();
+  ship.hit();
+  expect(ship.getShipHits()).toBe(2);
+  expect(ship.isSunk()).toBeFalsy();
+});
+
+test("create a ship with 2 length, hit it 2 times, increasing hits to 2 and sinking it", () => {
+  const ship = Ship(2);
+  ship.hit();
+  ship.hit();
+  expect(ship.getShipHits()).toBe(2);
+  expect(ship.isSunk()).toBeTruthy();
+});
+
+test("try to hit an already sunken ship", () => {
+  const ship = Ship(1);
+  ship.hit();
+  expect(ship.getShipHits()).toBe(1);
+  expect(ship.isSunk()).toBeTruthy();
+  expect(ship.hit()).toBe("That ship has already been sunk!");
+  expect(ship.getShipHits()).toBe(1);
+});
