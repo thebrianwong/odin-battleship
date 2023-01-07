44397a00ffd9b0db5af9b03d987af090cbd400d1
Brian Wong <brianw7218@gmail.com>
2023-01-06 21:40:00 -0800

    Create Ship factory function that satisfies tests

---
+++ src/ship.js
@@ -0,0 +1,29 @@
+const Ship = (givenLength) => {
+  const length = givenLength;
+  let hits = 0;
+  let sunk = false;
+  const getShipLength = () => length;
+  const getShipHits = () => hits;
+  const isSunk = () => sunk;
+  const checkIfSunk = () => {
+    if (hits === length) {
+      sunk = true;
+    }
+  };
+  const hit = () => {
+    if (!sunk) {
+      hits += 1;
+      checkIfSunk();
+    } else {
+      return "That ship has already been sunk!";
+    }
+  };
+  return {
+    getShipLength,
+    getShipHits,
+    isSunk,
+    hit,
+  };
+};
+
+module.exports = Ship;
