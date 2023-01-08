fb8791835163deca11fc5af9f95861b76d74239b
Brian Wong <brianw7218@gmail.com>
2023-01-07 21:58:44 -0800

    Change a test method name for consistency

--- tests/player.test.js
+++ tests/player.test.js
@@ -44,7 +44,7 @@ test("Player 1 can hit Player 2's Ship", () => {
   const player2Ship = player2Gameboard.getPlacedShips()[0];
   const spyPlayer2ReceiveAttack = jest.spyOn(player2Gameboard, "receiveAttack");
   const spyPlayer2ShipHit = jest.spyOn(player2Ship, "hit");
-  player1.initializeAttack(player2, [0, 0]);
+  player1.sendAttack(player2, [0, 0]);
   expect(spyPlayer2ReceiveAttack).toHaveBeenCalled();
   expect(spyPlayer2ShipHit).toHaveBeenCalled();
   expect(player2Gameboard.getHitShots()).toContain([0, 0]);
@@ -61,7 +61,7 @@ test("Player 1 misses Player 2's Ship", () => {
   const player2Ship = player2Gameboard.getPlacedShips()[0];
   const spyPlayer2ReceiveAttack = jest.spyOn(player2Gameboard, "receiveAttack");
   const spyPlayer2ShipHit = jest.spyOn(player2Ship, "hit");
-  player1.initializeAttack(player2, [0, 2]);
+  player1.sendAttack(player2, [0, 2]);
   expect(spyPlayer2ReceiveAttack).not.toHaveBeenCalled();
   expect(spyPlayer2ShipHit).not.toHaveBeenCalled();
   expect(player2Gameboard.getMissedShots()).toContain([0, 0]);
@@ -77,9 +77,9 @@ test("Player 1 sinks one of Player 2's Ships", () => {
   player2.addShipToGameboard(2, [0, 2], [0, 3]);
   const player2Ship1 = player2.getGameboard().getPlacedShips()[0];
   const player2Ship2 = player2.getGameboard().getPlacedShips()[1];
-  player1.initializeAttack(player2, [0, 0]);
+  player1.sendAttack(player2, [0, 0]);
   expect(player2Ship1.isSunk()).toBeFalsy();
-  player1.initializeAttack(player2, [0, 1]);
+  player1.sendAttack(player2, [0, 1]);
   expect(player2Ship1.isSunk()).toBeTruthy();
   expect(player2.getGameboard().isAllShipsSunk()).toBeFalsy();
   expect(player2Ship2.getShipHits()).toBe(0);
@@ -93,12 +93,12 @@ test("Player 1 sinks all of Player 2's Ships", () => {
   player2.addShipToGameboard(2, [0, 2], [0, 3]);
   const player2Ship1 = player2.getGameboard().getPlacedShips()[0];
   const player2Ship2 = player2.getGameboard().getPlacedShips()[1];
-  player1.initializeAttack(player2, [0, 0]);
-  player1.initializeAttack(player2, [0, 1]);
+  player1.sendAttack(player2, [0, 0]);
+  player1.sendAttack(player2, [0, 1]);
   expect(player2Ship1.isSunk()).toBeTruthy();
   expect(player2.getGameboard().isAllShipsSunk()).toBeFalsy();
-  player1.initializeAttack(player2, [0, 2]);
-  player1.initializeAttack(player2, [0, 3]);
+  player1.sendAttack(player2, [0, 2]);
+  player1.sendAttack(player2, [0, 3]);
   expect(player2Ship2.isSunk()).toBeTruthy();
   expect(player2.getGameboard().isAllShipsSunk()).toBeTruthy();
 });
