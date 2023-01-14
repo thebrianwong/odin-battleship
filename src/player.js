be947a890f79c89fcd01d9a86f04cec6450feeb3
Brian Wong <brianw7218@gmail.com>
2023-01-13 21:45:32 -0800

    Reorganize layout

--- src/player.js
+++ src/player.js
@@ -40,6 +40,129 @@ const Player = (playerType) => {
       opposingPlayer.sendComputerAttack();
     }
   };
+  const initializeComputerGameboard = () => {
+    if (!computerPlayer) {
+      return;
+    }
+    const lengthsOfShips = [5, 4, 3, 3, 2];
+    for (const shipLength of lengthsOfShips) {
+      const shipCoordinates = generateComputerShipCoordinates(shipLength);
+      addShipToGameboard(shipLength, shipCoordinates);
+    }
+  };
+  const sendComputerAttack = () => {
+    if (!computerPlayer) {
+      return;
+    }
+    const attackCoordinates = generateAttackCoordinates();
+    sendAttack(attackCoordinates);
+  };
+  const receiveAttack = (coordinates) => {
+    const attackResults = playerGameboard.receiveAttack(coordinates);
+    return attackResults;
+  };
+  const generateComputerShipCoordinates = (shipLength) => {
+    let listOfCoordinates = [];
+    do {
+      listOfCoordinates = [];
+      const startingCoordinates = generateStartingCoordinates();
+      const nonStartingCoordinates = generateNonStartingCoordinates(
+        startingCoordinates,
+        shipLength
+      );
+      listOfCoordinates.push(startingCoordinates);
+      listOfCoordinates = listOfCoordinates.concat(nonStartingCoordinates);
+    } while (listOfCoordinates.length < shipLength);
+    return listOfCoordinates;
+  };
+  const generateStartingCoordinates = () => {
+    const startingCoordinates = [undefined, undefined];
+    do {
+      startingCoordinates[0] = generateRandomCoordinate();
+      startingCoordinates[1] = generateRandomCoordinate();
+    } while (playerGameboard.getCoordinates(startingCoordinates) !== undefined);
+    return startingCoordinates;
+  };
+  const generateNonStartingCoordinates = (startingCoordinates, shipLength) => {
+    const possibleCoordinates = [];
+    if (checkIfValidEmptyCoordinates(startingCoordinates, shipLength, "up")) {
+      const upCoordinates = [];
+      for (let i = 1; i < shipLength; i++) {
+        upCoordinates.push([
+          startingCoordinates[0] - i,
+          startingCoordinates[1],
+        ]);
+      }
+      possibleCoordinates.push(upCoordinates);
+    }
+    if (
+      checkIfValidEmptyCoordinates(startingCoordinates, shipLength, "right")
+    ) {
+      const rightCoordinates = [];
+      for (let i = 1; i < shipLength; i++) {
+        rightCoordinates.push([
+          startingCoordinates[0],
+          startingCoordinates[1] + i,
+        ]);
+      }
+      possibleCoordinates.push(rightCoordinates);
+    }
+    if (checkIfValidEmptyCoordinates(startingCoordinates, shipLength, "down")) {
+      const downCoordinates = [];
+      for (let i = 1; i < shipLength; i++) {
+        downCoordinates.push([
+          startingCoordinates[0] + i,
+          startingCoordinates[1],
+        ]);
+      }
+      possibleCoordinates.push(downCoordinates);
+    }
+    if (checkIfValidEmptyCoordinates(startingCoordinates, shipLength, "left")) {
+      const leftCoordinates = [];
+      for (let i = 1; i < shipLength; i++) {
+        leftCoordinates.push([
+          startingCoordinates[0],
+          startingCoordinates[1] - i,
+        ]);
+      }
+      possibleCoordinates.push(leftCoordinates);
+    }
+    if (possibleCoordinates.length === 0) {
+      return possibleCoordinates;
+    }
+    const randomIndex = Math.floor(Math.random() * possibleCoordinates.length);
+    return possibleCoordinates[randomIndex];
+  };
+  const generateRandomCoordinate = () => {
+    const BOARDAXESLENGTH = 10;
+    return Math.floor(Math.random() * BOARDAXESLENGTH);
+  };
+  const checkIfPreviouslyAttacked = (
+    opposingPlayerReceivedShots,
+    attackCoordinates
+  ) =>
+    opposingPlayerReceivedShots.some(
+      (receivedShot) =>
+        receivedShot[0] === attackCoordinates[0] &&
+        receivedShot[1] === attackCoordinates[1]
+    );
+  const generateAttackCoordinates = () => {
+    const receivedMissedShots = opposingPlayer
+      .getGameboard()
+      .getReceivedMissedShots();
+    const receivedHitShots = opposingPlayer
+      .getGameboard()
+      .getReceivedHitShots();
+    const attackCoordinates = [undefined, undefined];
+    do {
+      attackCoordinates[0] = generateRandomCoordinate();
+      attackCoordinates[1] = generateRandomCoordinate();
+    } while (
+      checkIfPreviouslyAttacked(receivedMissedShots, attackCoordinates) ||
+      checkIfPreviouslyAttacked(receivedHitShots, attackCoordinates)
+    );
+    return attackCoordinates;
+  };
   const checkIfValidEmptyCoordinates = (
     startingCoordinates,
     shipLength,
@@ -113,129 +236,6 @@ const Player = (playerType) => {
       default:
         break;
     }
-  };
-  const generateRandomCoordinate = () => {
-    const BOARDAXESLENGTH = 10;
-    return Math.floor(Math.random() * BOARDAXESLENGTH);
-  };
-  const generateStartingCoordinates = () => {
-    const startingCoordinates = [undefined, undefined];
-    do {
-      startingCoordinates[0] = generateRandomCoordinate();
-      startingCoordinates[1] = generateRandomCoordinate();
-    } while (playerGameboard.getCoordinates(startingCoordinates) !== undefined);
-    return startingCoordinates;
-  };
-  const generateNonStartingCoordinates = (startingCoordinates, shipLength) => {
-    const possibleCoordinates = [];
-    if (checkIfValidEmptyCoordinates(startingCoordinates, shipLength, "up")) {
-      const upCoordinates = [];
-      for (let i = 1; i < shipLength; i++) {
-        upCoordinates.push([
-          startingCoordinates[0] - i,
-          startingCoordinates[1],
-        ]);
-      }
-      possibleCoordinates.push(upCoordinates);
-    }
-    if (
-      checkIfValidEmptyCoordinates(startingCoordinates, shipLength, "right")
-    ) {
-      const rightCoordinates = [];
-      for (let i = 1; i < shipLength; i++) {
-        rightCoordinates.push([
-          startingCoordinates[0],
-          startingCoordinates[1] + i,
-        ]);
-      }
-      possibleCoordinates.push(rightCoordinates);
-    }
-    if (checkIfValidEmptyCoordinates(startingCoordinates, shipLength, "down")) {
-      const downCoordinates = [];
-      for (let i = 1; i < shipLength; i++) {
-        downCoordinates.push([
-          startingCoordinates[0] + i,
-          startingCoordinates[1],
-        ]);
-      }
-      possibleCoordinates.push(downCoordinates);
-    }
-    if (checkIfValidEmptyCoordinates(startingCoordinates, shipLength, "left")) {
-      const leftCoordinates = [];
-      for (let i = 1; i < shipLength; i++) {
-        leftCoordinates.push([
-          startingCoordinates[0],
-          startingCoordinates[1] - i,
-        ]);
-      }
-      possibleCoordinates.push(leftCoordinates);
-    }
-    if (possibleCoordinates.length === 0) {
-      return possibleCoordinates;
-    }
-    const randomIndex = Math.floor(Math.random() * possibleCoordinates.length);
-    return possibleCoordinates[randomIndex];
-  };
-  const generateShipCoordinates = (shipLength) => {
-    let listOfCoordinates = [];
-    do {
-      listOfCoordinates = [];
-      const startingCoordinates = generateStartingCoordinates();
-      const nonStartingCoordinates = generateNonStartingCoordinates(
-        startingCoordinates,
-        shipLength
-      );
-      listOfCoordinates.push(startingCoordinates);
-      listOfCoordinates = listOfCoordinates.concat(nonStartingCoordinates);
-    } while (listOfCoordinates.length < shipLength);
-    return listOfCoordinates;
-  };
-  const initializeComputerGameboard = () => {
-    if (!computerPlayer) {
-      return;
-    }
-    const lengthsOfShips = [5, 4, 3, 3, 2];
-    for (const shipLength of lengthsOfShips) {
-      const shipCoordinates = generateShipCoordinates(shipLength);
-      addShipToGameboard(shipLength, shipCoordinates);
-    }
-  };
-  const checkIfPreviouslyAttacked = (
-    opposingPlayerReceivedShots,
-    attackCoordinates
-  ) =>
-    opposingPlayerReceivedShots.some(
-      (receivedShot) =>
-        receivedShot[0] === attackCoordinates[0] &&
-        receivedShot[1] === attackCoordinates[1]
-    );
-  const generateAttackCoordinates = () => {
-    const receivedMissedShots = opposingPlayer
-      .getGameboard()
-      .getReceivedMissedShots();
-    const receivedHitShots = opposingPlayer
-      .getGameboard()
-      .getReceivedHitShots();
-    const attackCoordinates = [undefined, undefined];
-    do {
-      attackCoordinates[0] = generateRandomCoordinate();
-      attackCoordinates[1] = generateRandomCoordinate();
-    } while (
-      checkIfPreviouslyAttacked(receivedMissedShots, attackCoordinates) ||
-      checkIfPreviouslyAttacked(receivedHitShots, attackCoordinates)
-    );
-    return attackCoordinates;
-  };
-  const sendComputerAttack = () => {
-    if (!computerPlayer) {
-      return;
-    }
-    const attackCoordinates = generateAttackCoordinates();
-    sendAttack(attackCoordinates);
-  };
-  const receiveAttack = (coordinates) => {
-    const attackResults = playerGameboard.receiveAttack(coordinates);
-    return attackResults;
   };
   return {
     isComputer,
