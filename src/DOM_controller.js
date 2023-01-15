3c5f0b9be2f3260443e800f44b766db37e14a40b
Brian Wong <brianw7218@gmail.com>
2023-01-14 22:35:47 -0800

    Rework function to create gameboard DOM elements to include column and row labels

--- src/DOM_controller.js
+++ src/DOM_controller.js
@@ -1,13 +1,34 @@
 const DOMController = (() => {
   const createBoardCells = (gameboardDOM) => {
     const BOARDAXESLENGTH = 10;
-    for (let i = 0; i < BOARDAXESLENGTH; i++) {
-      for (let j = 0; j < BOARDAXESLENGTH; j++) {
-        const cell = document.createElement("button");
-        cell.setAttribute("dataRow", i);
-        cell.setAttribute("dataColumn", j);
-        // some click event listener where attack is made OR prior to that, placing ship on that cell
-        gameboardDOM.appendChild(cell);
+    const columnLabels = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
+    const rowLabels = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+    for (let i = -1; i < BOARDAXESLENGTH; i++) {
+      for (let j = -1; j < BOARDAXESLENGTH; j++) {
+        if (i === -1) {
+          if (j === -1) {
+            const invisiblePlaceholderElement = document.createElement("div");
+            invisiblePlaceholderElement.classList.add("gameboard-cell");
+            invisiblePlaceholderElement.style.visibility = "hidden";
+            gameboardDOM.appendChild(invisiblePlaceholderElement);
+          } else {
+            const columnLabelElement = document.createElement("div");
+            columnLabelElement.classList.add("gameboard-cell");
+            columnLabelElement.textContent = columnLabels[j];
+            gameboardDOM.appendChild(columnLabelElement);
+          }
+        } else if (j === -1) {
+          const rowLabelElement = document.createElement("div");
+          rowLabelElement.classList.add("gameboard-cell");
+          rowLabelElement.textContent = rowLabels[i];
+          gameboardDOM.appendChild(rowLabelElement);
+        } else {
+          const cellElement = document.createElement("button");
+          cellElement.setAttribute("dataRow", i);
+          cellElement.setAttribute("dataColumn", j);
+          // some click event listener where attack is made OR prior to that, placing ship on that cell
+          gameboardDOM.appendChild(cellElement);
+        }
       }
     }
   };
