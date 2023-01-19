a7ece6349e9c32cb48389a32499b001cd2fd5cdd
Brian Wong <brianw7218@gmail.com>
2023-01-18 22:16:52 -0800

    Adjust tests to account for DOM manipulation and a player method that got refactored

--- tests/player.test.js
+++ tests/player.test.js
@@ -1,3 +1,7 @@
+/**
+ * @jest-environment jsdom
+ */
+
 import { Ship } from "../src/ship";
 import { Gameboard } from "../src/gameboard";
 import { Player } from "../src/player";
@@ -55,6 +59,16 @@ test("6) Player 1 can hit Player 2's Ship", () => {
   const player2Ship = player2Gameboard.getPlacedShips()[0];
   const spyPlayer2ReceiveAttack = jest.spyOn(player2Gameboard, "receiveAttack");
   const spyPlayer2ShipHit = jest.spyOn(player2Ship, "hit");
+
+  // Set up DOM elements for computer
+  const computerGameboardDOM = document.createElement("div");
+  computerGameboardDOM.classList.add("opponent-board");
+  document.body.appendChild(computerGameboardDOM);
+  const targetCellComputer = document.createElement("div");
+  targetCellComputer.dataset.row = "0";
+  targetCellComputer.dataset.column = "0";
+  computerGameboardDOM.appendChild(targetCellComputer);
+
   player1.sendAttack([0, 0]);
   expect(spyPlayer2ReceiveAttack).toHaveBeenCalled();
   expect(spyPlayer2ShipHit).toHaveBeenCalled();
@@ -63,6 +77,7 @@ test("6) Player 1 can hit Player 2's Ship", () => {
   expect(player2Gameboard.getReceivedHitShots()).toContainEqual([0, 0]);
   expect(player2Gameboard.getReceivedMissedShots()).not.toContainEqual([0, 0]);
   expect(player2Ship.getShipHits()).toBe(1);
+  document.body.innerHTML = "";
 });
 
 test("7) Player 1 misses Player 2's Ship", () => {
@@ -82,6 +97,16 @@ test("7) Player 1 misses Player 2's Ship", () => {
   const player2Ship = player2Gameboard.getPlacedShips()[0];
   const spyPlayer2ReceiveAttack = jest.spyOn(player2Gameboard, "receiveAttack");
   const spyPlayer2ShipHit = jest.spyOn(player2Ship, "hit");
+
+  // Set up DOM elements for computer
+  const computerGameboardDOM = document.createElement("div");
+  computerGameboardDOM.classList.add("opponent-board");
+  document.body.appendChild(computerGameboardDOM);
+  const targetCellComputer = document.createElement("div");
+  targetCellComputer.dataset.row = "0";
+  targetCellComputer.dataset.column = "2";
+  computerGameboardDOM.appendChild(targetCellComputer);
+
   player1.sendAttack([0, 2]);
   expect(spyPlayer2ReceiveAttack).toHaveBeenCalled();
   expect(spyPlayer2ShipHit).not.toHaveBeenCalled();
@@ -90,6 +115,7 @@ test("7) Player 1 misses Player 2's Ship", () => {
   expect(player2Gameboard.getReceivedMissedShots()).toContainEqual([0, 2]);
   expect(player2Gameboard.getReceivedHitShots()).not.toContainEqual([0, 2]);
   expect(player2Ship.getShipHits()).toBe(0);
+  document.body.innerHTML = "";
 });
 
 test("8) Player 1 sinks one of Player 2's Ships", () => {
@@ -110,6 +136,20 @@ test("8) Player 1 sinks one of Player 2's Ships", () => {
   ]);
   const player2Ship1 = player2.getGameboard().getPlacedShips()[0];
   const player2Ship2 = player2.getGameboard().getPlacedShips()[1];
+
+  // Set up DOM elements for computer
+  const computerGameboardDOM = document.createElement("div");
+  computerGameboardDOM.classList.add("opponent-board");
+  document.body.appendChild(computerGameboardDOM);
+  const targetCellComputer1 = document.createElement("div");
+  targetCellComputer1.dataset.row = "0";
+  targetCellComputer1.dataset.column = "0";
+  computerGameboardDOM.appendChild(targetCellComputer1);
+  const targetCellComputer2 = document.createElement("div");
+  targetCellComputer2.dataset.row = "0";
+  targetCellComputer2.dataset.column = "1";
+  computerGameboardDOM.appendChild(targetCellComputer2);
+
   player1.sendAttack([0, 0]);
   expect(player2Ship1.getShipHits()).toBe(1);
   expect(player2Ship1.isSunk()).toBeFalsy();
@@ -118,6 +158,7 @@ test("8) Player 1 sinks one of Player 2's Ships", () => {
   expect(player2Ship1.isSunk()).toBeTruthy();
   expect(player2.getGameboard().isAllShipsSunk()).toBeFalsy();
   expect(player2Ship2.getShipHits()).toBe(0);
+  document.body.innerHTML = "";
 });
 
 test("9) Player 1 sinks all of Player 2's Ships", () => {
@@ -140,6 +181,28 @@ test("9) Player 1 sinks all of Player 2's Ships", () => {
   const player2Ship2 = player2.getGameboard().getPlacedShips()[1];
   const spyShip1Hit = jest.spyOn(player2Ship1, "hit");
   const spyShip2Hit = jest.spyOn(player2Ship2, "hit");
+
+  // Set up DOM elements for computer
+  const computerGameboardDOM = document.createElement("div");
+  computerGameboardDOM.classList.add("opponent-board");
+  document.body.appendChild(computerGameboardDOM);
+  const targetCellComputer1 = document.createElement("div");
+  targetCellComputer1.dataset.row = "0";
+  targetCellComputer1.dataset.column = "0";
+  computerGameboardDOM.appendChild(targetCellComputer1);
+  const targetCellComputer2 = document.createElement("div");
+  targetCellComputer2.dataset.row = "0";
+  targetCellComputer2.dataset.column = "1";
+  computerGameboardDOM.appendChild(targetCellComputer2);
+  const targetCellComputer3 = document.createElement("div");
+  targetCellComputer3.dataset.row = "0";
+  targetCellComputer3.dataset.column = "2";
+  computerGameboardDOM.appendChild(targetCellComputer3);
+  const targetCellComputer4 = document.createElement("div");
+  targetCellComputer4.dataset.row = "0";
+  targetCellComputer4.dataset.column = "3";
+  computerGameboardDOM.appendChild(targetCellComputer4);
+
   player1.sendAttack([0, 0]);
   player1.sendAttack([0, 1]);
   expect(spyShip1Hit).toHaveBeenCalledTimes(2);
@@ -150,6 +213,7 @@ test("9) Player 1 sinks all of Player 2's Ships", () => {
   expect(spyShip2Hit).toHaveBeenCalledTimes(2);
   expect(player2Ship2.isSunk()).toBeTruthy();
   expect(player2.getGameboard().isAllShipsSunk()).toBeTruthy();
+  document.body.innerHTML = "";
 });
 
 test("10) Computer Player can place their 5 Ships of respective lengths on the Gameboard", () => {
@@ -174,6 +238,16 @@ test("11) Computer Player can send attacks that hit", () => {
   const humanGameboard = humanPlayer.getGameboard();
   const spyHumanReceiveAttack = jest.spyOn(humanGameboard, "receiveAttack");
   jest.spyOn(Math, "random").mockReturnValue(0.1);
+
+  // Set up DOM elements for human
+  const humanGameboardDOM = document.createElement("div");
+  humanGameboardDOM.classList.add("player-board");
+  document.body.appendChild(humanGameboardDOM);
+  const targetCellHuman = document.createElement("div");
+  targetCellHuman.dataset.row = "1";
+  targetCellHuman.dataset.column = "1";
+  humanGameboardDOM.appendChild(targetCellHuman);
+
   computerPlayer.sendComputerAttack();
   expect(spyHumanReceiveAttack).toHaveBeenCalled();
   expect(computerGameboard.getSentMissedShots().length).toBe(0);
@@ -181,6 +255,7 @@ test("11) Computer Player can send attacks that hit", () => {
   expect(humanGameboard.getReceivedMissedShots().length).toBe(0);
   expect(humanGameboard.getReceivedHitShots().length).toBe(1);
   expect(humanGameboard.getPlacedShips()[0].getShipHits()).toBe(1);
+  document.body.innerHTML = "";
   jest.spyOn(global.Math, "random").mockRestore();
 });
 
@@ -197,6 +272,16 @@ test("12) Computer Player can send attacks that miss", () => {
   const humanGameboard = humanPlayer.getGameboard();
   const spyHumanReceiveAttack = jest.spyOn(humanGameboard, "receiveAttack");
   jest.spyOn(Math, "random").mockReturnValue(0.1);
+
+  // Set up DOM elements for human
+  const humanGameboardDOM = document.createElement("div");
+  humanGameboardDOM.classList.add("player-board");
+  document.body.appendChild(humanGameboardDOM);
+  const targetCellHuman = document.createElement("div");
+  targetCellHuman.dataset.row = "1";
+  targetCellHuman.dataset.column = "1";
+  humanGameboardDOM.appendChild(targetCellHuman);
+
   computerPlayer.sendComputerAttack();
   expect(spyHumanReceiveAttack).toHaveBeenCalled();
   expect(computerGameboard.getSentMissedShots().length).toBe(1);
@@ -204,6 +289,7 @@ test("12) Computer Player can send attacks that miss", () => {
   expect(humanGameboard.getReceivedMissedShots().length).toBe(1);
   expect(humanGameboard.getReceivedHitShots().length).toBe(0);
   expect(humanGameboard.getPlacedShips()[0].getShipHits()).toBe(0);
+  document.body.innerHTML = "";
   jest.spyOn(global.Math, "random").mockRestore();
 });
 
@@ -228,7 +314,26 @@ test("13) Computer Player is unable to send attacks to coordinates previously hi
     "getReceivedHitShots"
   );
   jest.spyOn(Math, "random").mockReturnValue(0.5);
+
+  // Set up DOM elements for human
+  const humanGameboardDOM = document.createElement("div");
+  humanGameboardDOM.classList.add("player-board");
+  document.body.appendChild(humanGameboardDOM);
+  const targetCellHuman1 = document.createElement("div");
+  targetCellHuman1.dataset.row = "5";
+  targetCellHuman1.dataset.column = "5";
+  humanGameboardDOM.appendChild(targetCellHuman1);
+  const targetCellHuman2 = document.createElement("div");
+  targetCellHuman2.dataset.row = "0";
+  targetCellHuman2.dataset.column = "0";
+  humanGameboardDOM.appendChild(targetCellHuman2);
+
   computerPlayer.sendComputerAttack();
+  // Each gets called in pairs when the computer sends a valid attack
+  // +1 for creating an attack coordinate, +1 for sending the attack
+  expect(spyHumanGetReceivedMissedShots).toHaveBeenCalledTimes(2);
+  expect(spyHumanGetReceivedHitShots).toHaveBeenCalledTimes(2);
+
   expect(spyHumanReceiveAttack).toHaveBeenCalled();
   expect(computerGameboard.getSentMissedShots().length).toBe(0);
   expect(computerGameboard.getSentHitShots().length).toBe(1);
@@ -241,11 +346,16 @@ test("13) Computer Player is unable to send attacks to coordinates previously hi
     .mockReturnValueOnce(0.5)
     .mockReturnValueOnce(0.5);
   computerPlayer.sendComputerAttack();
-  expect(spyHumanGetReceivedMissedShots).toHaveBeenCalledTimes(2);
-  expect(spyHumanGetReceivedHitShots).toHaveBeenCalledTimes(2);
+
+  // The computer tries to get an invalid attack coordinate (+1),
+  // then tries again (+1), resulting in a valid attack coordinate
+  // that gets sent (+1)
+  expect(spyHumanGetReceivedMissedShots).toHaveBeenCalledTimes(5);
+  expect(spyHumanGetReceivedHitShots).toHaveBeenCalledTimes(5);
   expect(spyHumanReceiveAttack).toHaveBeenCalledTimes(2);
   expect(computerGameboard.getSentMissedShots().length).toBe(1);
   expect(computerGameboard.getSentHitShots().length).toBe(1);
   expect(humanGameboard.getPlacedShips()[0].getShipHits()).toBe(1);
+  document.body.innerHTML = "";
   jest.spyOn(global.Math, "random").mockRestore();
 });
